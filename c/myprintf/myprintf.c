f888d18151e693c4c404f19cf206726757bde166
wanglei <oscenter.lei@gmail.com>
2010-07-05 14:01:59 +0800

    add myprintf function for c.

---
+++ c/myprintf/myprintf.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+void myprintf(const char *format, ...)
+{
+	va_list ap;
+	char c;
+	va_start(ap, format);
+	while(c = *format++)
+	{
+		switch (c)
+		{
+			case 'c':
+				{
+					char ch = va_arg(ap, int);
+					putchar(ch + 48);
+					break;
+				}
+			case 's':
+				{
+					char *p =va_arg(ap, char *);
+					fputs(p, stdout);
+					break;
+				}
+			default :
+				putchar(c);
+		}
+	}
+
+	va_end(ap);
+}
+
+
+int main(void)
+{
+	int i = 1;
+	char *ch = "hello";
+	myprintf(" i = c\t s\n", i, ch);
+	return 0;
+}
